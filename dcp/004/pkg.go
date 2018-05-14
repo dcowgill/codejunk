069bbc11c29383e4836b26bb817af67dd9396433
Daniel Cowgill <dcowgill@gmail.com>
2018-05-14 13:00:50 -0400

    Add dcp no. 4

---
+++ dcp/004/pkg.go
@@ -0,0 +1,82 @@
+/*
+
+Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Stripe.
+
+Given an array of integers, find the first missing positive integer in
+linear time and constant space. In other words, find the lowest positive
+integer that does not exist in the array. The array can contain
+duplicates and negative numbers as well.
+
+For example, the input [3, 4, -1, 1] should give 2. The input [1, 2, 0]
+should give 3.
+
+You can modify the input array in-place.
+
+*/
+package dcp004
+
+import (
+	"sort"
+)
+
+// Ugly but straightforward. Sorts the list, so O(n*log(n)).
+func refImpl(a []int) int {
+	sort.Ints(a)
+	a = posTail(a)
+	x := 1
+	i := 0
+	for i < len(a) {
+		j := i
+		for j < len(a) && a[j] == x {
+			j++
+		}
+		if j == i {
+			return x
+		}
+		i = j
+		x = x + 1
+	}
+	return x
+}
+
+// Assumes a is sorted.
+// Returns the tail of a containing positive ints.
+func posTail(a []int) []int {
+	for i, x := range a {
+		if x > 0 {
+			return a[i:]
+		}
+	}
+	return nil
+}
+
+// We know a priori the answer must be in the range [1, len(a)].
+// Therefore we can use the array itself to keep track of whether we
+// have encountered each integer. Care must be taken to ignore both
+// non-positive and duplicate integers.
+func linear(a []int) int {
+	for i := 0; i < len(a); i++ {
+		x := a[i]
+		if x <= 0 || x >= len(a) {
+			continue // value not in range; ignore
+		}
+		j := x - 1 // target slot for a[i]
+		if a[j] == x {
+			// The target is already set to its correct value, which
+			// means a[i] is a duplicate. Set to zero to ignore.
+			a[i] = 0
+			continue
+		}
+		a[j], a[i] = a[i], a[j]
+		i-- // restart loop at same position
+	}
+	// Report the first slot that does not contain its corresponding value.
+	for i, x := range a {
+		if x != i+1 {
+			return i + 1
+		}
+	}
+	return 0
+}
---
+++ dcp/004/pkg_test.go
@@ -0,0 +1,37 @@
+package dcp004
+
+import (
+	"fmt"
+	"testing"
+)
+
+var tests = []struct {
+	a []int
+	n int
+}{
+	{[]int{3, 4, -1, 1}, 2},
+	{[]int{1, 2, 0}, 3},
+	{[]int{2, 4, 3, 4, 2, 4, 3, 2}, 1},
+	{[]int{0, -1, -2, -3, 0, -4}, 1},
+}
+
+var funcs = []struct {
+	name string
+	f    func([]int) int
+}{
+	{"refImpl", refImpl},
+	{"linear", linear},
+}
+
+func TestAll(t *testing.T) {
+	for _, fn := range funcs {
+		for _, tt := range tests {
+			t.Run(fmt.Sprintf("%s(%+v)", fn.name, tt.a), func(t *testing.T) {
+				n := fn.f(tt.a)
+				if n != tt.n {
+					t.Fatalf("got %d, want %d", n, tt.n)
+				}
+			})
+		}
+	}
+}
