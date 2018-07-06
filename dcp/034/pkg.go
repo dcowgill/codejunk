776e2564eeef46ab4972674a4cc2a7f22c12f619
Daniel Cowgill <dcowgill@gmail.com>
2018-07-06 12:52:02 -0400

    dcp/034: use dynamic programming

--- dcp/034/pkg.go
+++ dcp/034/pkg.go
@@ -22,29 +22,44 @@ package dcp034
 // minPalindrome returns the palindrome formed by making the fewest insertions
 // into s. In case of ties, chooses the lexicographically least palindrome.
 func minPalindrome(s string) string {
+	type key struct {
+		a string // current string
+		r string // prefix+suffix
+		x int    // moves required
+	}
+	var cache = make(map[key]result)
+	var rec func(a string, r string, x int) result
+	rec = func(a string, r string, x int) result {
+		// Termination condition.
+		n := len(a)
+		if n <= 1 {
+			return result{r + a + r, x}
+		}
+		// Check the cache.
+		k := key{a, r, x}
+		if result, ok := cache[k]; ok {
+			return result
+		}
+		// Compare a[i+1...j], a[i...j-1], and a[i+1...j+1].
+		best := rec(a[1:], a[:1], 1)
+		{
+			other := rec(a[:n-1], a[n-1:], 1)
+			if other.beats(best) {
+				best = other
+			}
+		}
+		if a[0] == a[n-1] {
+			other := rec(a[1:n-1], a[:1], 0)
+			if other.beats(best) {
+				best = other
+			}
+		}
+		// Memoize the answer and return.
+		answer := result{r + best.s + r, best.n + x}
+		cache[k] = answer
+		return answer
+	}
 	return rec(s, "", 0).s
-}
-
-// Recursive implementation of minPalindrome.
-func rec(a string, r string, x int) result {
-	n := len(a)
-	if n <= 1 {
-		return result{r + a + r, x}
-	}
-	best := rec(a[1:], a[:1], 1)
-	{
-		other := rec(a[:n-1], a[n-1:], 1)
-		if other.beats(best) {
-			best = other
-		}
-	}
-	if a[0] == a[n-1] {
-		other := rec(a[1:n-1], a[:1], 0)
-		if other.beats(best) {
-			best = other
-		}
-	}
-	return result{r + best.s + r, best.n + x}
 }
 
 // Intermediate result produced by rec().
