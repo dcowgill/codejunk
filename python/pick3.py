7569776f707b6623118b821703ed8d6081eca902
Daniel Cowgill <dcowgill@gmail.com>
2018-10-31 10:01:03 -0400

    pick3: cmdline args

--- python/pick3.py
+++ python/pick3.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
 import random
+import sys
 
-def pick3(num_games = 15):
+def pick3(num_games):
     games = sorted(shuffled(range(1, num_games+1))[:3])
     return [(n, random.choice(['Home', 'Away'])) for n in games]
 
@@ -12,7 +13,8 @@ def shuffled(seq):
     return seq
 
 def main():
-    print(pick3())
+    num_games = int(sys.argv[1]) if len(sys.argv) == 2 else 14
+    print(pick3(num_games))
 
 if __name__ == '__main__':
     main()
